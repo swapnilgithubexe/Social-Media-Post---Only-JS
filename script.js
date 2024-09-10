afb22fef794a908d32710d63057bafff8f82d6bc
swapnilgithubexe <swapnildutta2002@gmail.com>
2024-09-10 16:32:22 +0530

    updates

--- script.js
+++ script.js
@@ -43,14 +43,16 @@ function renderPosts() {
   commentsContainer.classList.add('comments-container');
   commentsContainer.style.display = 'none';
 
-  post1.comments.forEach((comment) => {
-    const commentElement = document.createElement('p');
-    commentElement.textContent = comment;
-    commentsContainer.appendChild(commentElement);
-  });
+  function renderComments() {
+    commentsContainer.innerHTML = "";
+    post1.comments.forEach((comment) => {
+      let commentElement = document.createElement('p');
+      commentElement.textContent = comment;
+      commentsContainer.appendChild(commentElement);
+    });
+  }
 
   postElement.appendChild(authorElement);
-
   postElement.appendChild(imageElement);
   postElement.appendChild(contentElement);
   postElement.appendChild(likeButton);
@@ -59,28 +61,48 @@ function renderPosts() {
   postElement.appendChild(postFooter);
   postElement.appendChild(commentsContainer);
 
-  postFooter.addEventListener('click', () => {
-    if (commentsContainer.style.display === 'none') {
-      commentsContainer.style.display = 'block';
-    } else {
-      commentsContainer.style.display = 'none';
-    }
-  });
+
 
   postsContainer.appendChild(postElement);
+  postFooter.addEventListener('click', () => {
+    commentsContainer.style.display = commentsContainer.style.display === 'none' ? 'block' : 'none';
+  });
 
-  let clickCount = 0;
+  let clickCount = 0
   likeButton.addEventListener("click", () => {
     clickCount += 1
     if (clickCount <= 1) {
-      post1.likes += 1
-      postFooter.textContent = `Likes: ${post1.likes}   Comments: ${post1.comments.length}`;
-      likeButton.style.backgroundColor = "red";
+      likePost(1)
+    }
+  })
 
+  commentButton.addEventListener("click", () => {
+    let commentContent = commentInput.value;
+    if (commentContent.trim() !== "") {
+      addComment(1, commentContent)
+      renderComments();
+      commentInput.value = "";
     }
   })
 }
 
+const likePost = (id) => {
+  if (post1.id === id) {
+    post1.likes += 1;
+    const postFooter = document.querySelector('.post-footer');
+    postFooter.textContent = `Likes: ${post1.likes}   Comments: ${post1.comments.length}`;
+    let likeButton = document.querySelector(".like-button");
+    likeButton.style.backgroundColor = "red";
+  }
 
+};
+
+const addComment = (id, comment) => {
+  if (post1.id === id) {
+    post1.comments.push(comment);
+    const postFooter = document.querySelector('.post-footer');
+    postFooter.textContent = `Likes: ${post1.likes}   Comments: ${post1.comments.length}`;
+  }
+}
 
 renderPosts();    
